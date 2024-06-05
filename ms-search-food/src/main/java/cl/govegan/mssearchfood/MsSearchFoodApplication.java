bfcf2f3222959478bdaa34fe98cd78a57c6b3ea7
Gab-Serrano <gab.serrano@duocuc.cl>
2024-06-05 06:15:20 -0400

    fix cors

--- ms-search-food/src/main/java/cl/govegan/mssearchfood/MsSearchFoodApplication.java
+++ ms-search-food/src/main/java/cl/govegan/mssearchfood/MsSearchFoodApplication.java
@@ -2,6 +2,10 @@ package cl.govegan.mssearchfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsSearchFoodApplication {
@@ -9,4 +13,19 @@ public class MsSearchFoodApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsSearchFoodApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8100", "http://localhost:8080")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(false);
+            }
+        };
+    }
+
 }
