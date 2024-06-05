90ce96b0393c279e259b2c83c144ab36d5d1ad4d
Gab-Serrano <gab.serrano@duocuc.cl>
2024-06-05 03:06:26 -0400

    agrega rest cors

--- ms-search-food/src/main/java/cl/govegan/mssearchfood/MsSearchFoodApplication.java
+++ ms-search-food/src/main/java/cl/govegan/mssearchfood/MsSearchFoodApplication.java
@@ -3,30 +3,27 @@ package cl.govegan.mssearchfood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsSearchFoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsSearchFoodApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsSearchFoodApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(@NonNull CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8100", "http://localhost:8100/", "http://localhost:8080",
-								"http://localhost:8080/")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("*")
-						.allowCredentials(false);
-			}
-		};
-	}
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8100", "http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
--- ms-search-food/src/main/java/cl/govegan/mssearchfood/controller/recipecontroller/RecipeController.java
+++ ms-search-food/src/main/java/cl/govegan/mssearchfood/controller/recipecontroller/RecipeController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +19,14 @@ import cl.govegan.mssearchfood.utils.responses.ResponseHttp;
 
 @RestController
 @RequestMapping("/api/v1/recipes")
+@CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8080"})
 public class RecipeController {
 
     @Autowired
     private RecipeService recipeService;
 
     @GetMapping()
+    @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8080"})
     public ResponseEntity<ResponseHttp<Page<Recipe>>> findAllRecipes(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
