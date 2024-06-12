7386f98c613e76b4591b9e78c16aa0f8e9281596
Gab-Serrano <gab.serrano@duocuc.cl>
2024-06-12 12:45:39 -0400

    #

--- ms-search-food/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Obtener el secreto de Google Cloud Secret Manager
-MONGO_URI=$(gcloud secrets versions access latest --secret=mongodb_uri_ms-search-food)
-export MONGO_URI
-
-# Ejecutar la aplicación
-java -Dlogging.file.path=/app/logs -jar ms-search-food.jar
